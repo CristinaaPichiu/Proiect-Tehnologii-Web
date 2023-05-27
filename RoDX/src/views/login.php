50654c72e041a7dadc3319362da56a8e38f0b66a
PichiuCristina <cristinapichiu38@gmail.com>
2023-05-27 11:56:24 +0300

    aaaaa

--- RoDX/src/views/login.php
+++ RoDX/src/views/login.php
@@ -6,22 +6,28 @@ $pass = "J5vORyk7ysgEBeHVnEJhD9Hnywf6kfm6";
 $db = "cyjvryeg";
 
 $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die("Could not connect to Server\n");
+echo "Sunt aici";
 
-if (!$con) {
+$query = "SELECT * FROM users";
+    $result = pg_query($con, $query);
+
+if (pg_num_rows($result) == 0) {
     echo "Error: Unable to open database\n";
 } else {
     if (isset($_POST['username']) && isset($_POST['password'])) {
+
         $name = $_POST['username'];
         $password = $_POST['password'];
 
         $query = "INSERT INTO register (username, password) VALUES ('$name', '$password')";
         $result = pg_query($con, $query);
         if ($result) {
-            header("Location: http://localhost/views/index.html");
+           echo "lala";
             exit(); 
         } else {
             echo "Error: Unable to insert data into the database\n";
         }
+        echo "sunt in if";
     }
 }
 pg_close($con);
