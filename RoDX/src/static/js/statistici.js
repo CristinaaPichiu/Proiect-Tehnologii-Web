b28d7bacb43a92a504e382fcc04738a7761899a3
PichiuCristina <cristinapichiu38@gmail.com>
2023-06-15 16:47:34 +0300

    filtre

--- RoDX/src/static/js/statistici.js
+++ RoDX/src/static/js/statistici.js
@@ -1,42 +1,15 @@
-var button = document.getElementById("filter-button");
-var container = document.getElementById("filter-container");
-var input = document.querySelectorAll("input");
+// Show filters
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-button.onclick = function (e) {
-  e.stopPropagation();
-  if (container.classList.contains("filters--active")) {
-    container.classList.remove("filters--active");
-  } else {
-    container.classList.add("filters--active");
-  }
-};
-
-container.onclick = function (e) {
-  e.stopPropagation();
-};
-
-window.onclick = function () {
-  container.classList.remove("filters--active");
-};
-
-console.log(input);
-
-for (var i = 0; i < input.length; i++) {
-  var currentInput = input[i];
-
-  currentInput.onclick = function () {
-    var isChecked = false;
-    for (var j = 0; j < input.length; j++) {
-      if (input[j].checked) {
-        isChecked = true;
-        break;
-      }
-    }
-
-    if (isChecked) {
-      button.classList.add("button--highlight");
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      button.classList.remove("button--highlight");
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
-  };
+  });
 }
