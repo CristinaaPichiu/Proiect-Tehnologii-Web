13bceee49cc4ae98429edd93a388ba5c55a7db81
Bianca Bojescu <bianca.bojescu@gmail.com>
2023-06-16 12:34:35 +0300

    update register.js

--- RoDX/src/static/js/register.js
+++ RoDX/src/static/js/register.js
@@ -2,12 +2,9 @@
 const usernameTag = "name";
 const passwordTag = "password";
 const passwordTag1 = "firstPassword";
-const passwordTag2 = "secondPassword";
 const emailTag = "email";
-const accessTokenTag = "access_token";
-const receivedAccessTokenTag = "token";
+
 const async = true;
-const urlLogin = 'http://localhost:8081/api/auth/login';
 const urlRegister = 'http://localhost:8081/api/auth/register';
 
 
@@ -23,19 +20,6 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove("move-panel");
   // console.log("a2");
-});
-
-
-// Register
-const registerFormElement = document.getElementById("registerForm");
-registerFormElement.addEventListener('submit', e => {
-  e.preventDefault();
-  // console.log("submited Register!");
-
-  var registerCredentials = getRegisterCredentials(registerFormElement);
-  if (checkRegisterCredentials(registerCredentials)) {
-    register(registerCredentials);
-  }
 });
 
 /**
@@ -58,48 +42,6 @@ function register(registerCredentials) {
   };
   var body = JSON.stringify({ username: registerCredentials[usernameTag], user_password: registerCredentials[passwordTag1], user_email: registerCredentials[emailTag] });
   xhr.send(body);
-}
-
-/**
- * method used to get the register credentials
- * @param {*} registerFormElement 
- * @returns 
- */
-function getRegisterCredentials(registerFormElement) {
-  var credentials = {};
-  credentials[usernameTag] = registerFormElement[0].value;
-  credentials[emailTag] = registerFormElement[1].value;
-  credentials[passwordTag1] = registerFormElement[2].value;
-  credentials[passwordTag2] = registerFormElement[3].value;
-  return credentials;
-}
-
-/**
- * method used to check the register credentials
- * @param {*} registerCredentials 
- */
-function checkRegisterCredentials(credentials) {
-  if (credentials[usernameTag].length < 6) {
-    cleanHtml();
-    document.getElementById("username-constraints").innerHTML = "Username-ul trebuie sa contina sase sau mai multe caractere!";
-    return false;
-  }
-  if (credentials[emailTag] == "") {
-    cleanHtml();
-    document.getElementById("email-constraints").innerHTML = "Email-ul trebuie sa fie valid!";
-    return false;
-  }
-  if (credentials[passwordTag1].length < 6) {
-    cleanHtml();
-    document.getElementById("password-constraints").innerHTML = "Parola trebuie sa contina sase sau mai multe caractere!";
-    return false;
-  }
-  if (credentials[passwordTag1] !== credentials[passwordTag2]) {
-    cleanHtml();
-    document.getElementById("password-constraints").innerHTML = "Parola trebuie sa fie identica!";
-    return false;
-  }
-  return true;
 }
 
 function cleanHtml() {
