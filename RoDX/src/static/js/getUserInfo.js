23dd2cfb80f4e41a0d4a7af9e2e3b1d9ed516085
Bianca Bojescu <bianca.bojescu@gmail.com>
2023-06-24 02:38:37 +0300

    modal

--- RoDX/src/static/js/getUserInfo.js
+++ RoDX/src/static/js/getUserInfo.js
@@ -14,7 +14,7 @@ function loadUserInfo() {
       document.getElementById('password-modal').textContent = response.password;
       
       // Deschideți modalul
-      openModal();
+      openModalF();
       },
       error: function() {
         console.log("Nu se pot obține informațiile utilizatorului.");
@@ -22,8 +22,22 @@ function loadUserInfo() {
     });
   }
   
-  function openModal() {
+  function openModalF() {
+    
     var modal = document.getElementById('modal');
-    modal.style.display = 'block';
+    var openModal = document.getElementById("open-modal");
+    var closeModal = document.getElementsByClassName("close")[0];
+    openModal.onclick = function() {
+      modal.style.display = "block";
+    }
+    closeModal.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
   
